a8c651d86b64823ba392af3616c23c7ecc40ba57
Juliana Novoa <julianansolano@gmail.com>
2026-05-02 12:23:47 -0500

    Implementacion vista roles de editor y lecot

--- src/app/pages/editar-procesos/editar-procesos.ts
+++ src/app/pages/editar-procesos/editar-procesos.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-editar-procesos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './editar-procesos.html',
-  styleUrl: './editar-procesos.css',
+  styleUrl: './editar-procesos.css'
 })
-export class EditarProcesos {}
+export class EditarProcesos implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Aquí podrías validar que solo entren editores si quieres seguridad extra
+  }
+
+  navegarAProcesos(): void {
+    this.router.navigate(['/gestor-procesos']);
+  }
+
+  navegarARoles(): void {
+    this.router.navigate(['/gestor-roles']);
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/log-in']);
+  }
+}
