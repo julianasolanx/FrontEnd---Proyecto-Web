08b7f20ec89919b7896cd58ee622d57464b21b54
Juliana Novoa <julianansolano@gmail.com>
2026-05-02 11:13:46 -0500

    Primer version del canvas

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,44 +3,43 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'log-in',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'registrar-empresa',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'dashboard-general',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'consultar-procesos',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'editar-procesos',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'gestor-procesos',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'gestor-roles',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
   {
     path: 'gestor-usuarios',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   },
+
+  { path: 'modelador-proceso', renderMode: RenderMode.Client },
   { path: 'modelador-proceso/:id', renderMode: RenderMode.Client },
   { path: 'formulario-proceso', renderMode: RenderMode.Client },
-  { path: 'formulario-proceso/:id', renderMode: RenderMode.Client }
-
-  
-  
+  { path: 'formulario-proceso/:id', renderMode: RenderMode.Client },
 ];
